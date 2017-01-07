bd2e5e230b915fa83aff54010aa59c901fe90428
Dmitry Isaev <x@x128.ru>
2017-01-07 23:40:17 +0300

    [Assembler::Geometry] Degrees instead of radians

--- Assembler.js
+++ Assembler.js
@@ -4,9 +4,9 @@ var Geometry = {
     Chair : [
         { type : mushroominator.DetailGeometry.Square, pos : [0, 0, 0], rotation : [0, 0, 0] },
         { type : mushroominator.DetailGeometry.Square, pos : [0, 450, 0], rotation : [0, 0, 0] },
-        { type : mushroominator.DetailGeometry.Square, pos : [225, 0, 225], rotation : [0, Math.PI / 2, 0] },
-        { type : mushroominator.DetailGeometry.Square, pos : [-225, 0, 225], rotation : [0, Math.PI / 2, 0] },
-        { type : mushroominator.DetailGeometry.Square, pos : [0, 225, 225], rotation : [Math.PI / 2, 0, 0] },
+        { type : mushroominator.DetailGeometry.Square, pos : [225, 0, 225], rotation : [0, 90, 0] },
+        { type : mushroominator.DetailGeometry.Square, pos : [-225, 0, 225], rotation : [0, 90, 0] },
+        { type : mushroominator.DetailGeometry.Square, pos : [0, 225, 225], rotation : [90, 0, 0] },
     ]
 };
 
@@ -14,15 +14,16 @@ function create(geometry) {
     var creation = new THREE.Object3D();
     creation.selectableObjects = [];
 
+    var deg2rad = Math.PI / 180;
     for (var i = 0; i < geometry.length; i++) {
         var args = geometry[i];
         var detail = mushroominator.bake(args.type);
         detail.position.x = args.pos[0];
         detail.position.y = args.pos[1];
         detail.position.z = args.pos[2];
-        detail.rotateX(args.rotation[0]);
-        detail.rotateY(args.rotation[1]);
-        detail.rotateZ(args.rotation[2]);
+        detail.rotateX(args.rotation[0] * deg2rad);
+        detail.rotateY(args.rotation[1] * deg2rad);
+        detail.rotateZ(args.rotation[2] * deg2rad);
         creation.add(detail);
         creation.selectableObjects.extend(detail.selectableObjects); // FIXME: who exactly must define the selection logic?
     }
